082b53c0d615c1b1925c4e60d0e1dc0841541bd6
Sean Dodson <dodsonsean@comcast.net>
2020-08-22 10:46:06 -0400

    Implement timer function

--- src/cron-function/submit-form-timer/index.ts
+++ src/cron-function/submit-form-timer/index.ts
@@ -1,13 +1,16 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from '@azure/functions';
+import submitForm from 'su-covid-daily/submit-form';
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
-        context.log('Timer function is running late!');
-    }
-    context.log('Timer trigger function ran!', timeStamp);   
+  context.log('Starting daily form submission...', new Date().toISOString());
+
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
+  const receiptPath = 'dist/receipt.png';
+
+  submitForm(username, password, receiptPath);
+
+  context.log('Finished daily form submission.', new Date().toISOString());
 };
 
 export default timerTrigger;
