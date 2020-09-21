388d9273f6573c54a5ca644404e1a07d417624f5
Sean Dodson <dodsonsean@comcast.net>
2020-09-21 19:53:26 -0400

    Add update notification

--- src/cli/src/sucovid.ts
+++ src/cli/src/sucovid.ts
@@ -1,32 +1,48 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import UpdateNotifier from 'update-notifier';
 
-const version = '1.0.11';
+const version = '1.0.12';
 
-const program = new Command();
+(async () => {
+  const notifier = UpdateNotifier({
+    shouldNotifyInNpmScript: true,
+    updateCheckInterval: 1,
+    pkg: {
+      name: 'su-covid-daily-cli',
+      version: version,
+    },
+  });
 
-program
-  .name('sucovid')
-  .version(version, '-v, --version', 'output the current version')
-  .description(
-    'quickly submit daily COVID symptoms to the Stevenson University Wellness Center'
-  )
-  .command(
-    'submit',
-    'submit daily COVID symptoms to the Stevenson University Wellness Center'
-  )
-  .command(
-    'schedule',
-    'schedule the submission command to repeatedly run at a daily time'
-  )
-  .command(
-    'login',
-    'save credentials for the Stevenson University Wellness Center'
-  )
-  .command(
-    'logout',
-    'remove credentials for the Stevenson University Wellness Center'
-  );
+  await notifier.fetchInfo();
 
-program.parse();
+  notifier.notify({ isGlobal: true });
+
+  const program = new Command();
+
+  program
+    .name('sucovid')
+    .version(version, '-v, --version', 'output the current version')
+    .description(
+      'quickly submit daily COVID symptoms to the Stevenson University Wellness Center'
+    )
+    .command(
+      'submit',
+      'submit daily COVID symptoms to the Stevenson University Wellness Center'
+    )
+    .command(
+      'schedule',
+      'schedule the submission command to repeatedly run at a daily time'
+    )
+    .command(
+      'login',
+      'save credentials for the Stevenson University Wellness Center'
+    )
+    .command(
+      'logout',
+      'remove credentials for the Stevenson University Wellness Center'
+    );
+
+  program.parse();
+})();
