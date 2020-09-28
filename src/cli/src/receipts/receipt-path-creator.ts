0ec6ddfc0dbbb8fecb319572658133d20c2e8a58
Sean Dodson <dodsonsean@comcast.net>
2020-09-28 15:14:16 -0400

    Fix receipt characters

--- src/cli/src/receipts/receipt-path-creator.ts
+++ src/cli/src/receipts/receipt-path-creator.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import moment from 'moment';
 
 function createReceiptPath(directory: string): string {
-  const timeStamp = moment().format();
+  const timeStamp = moment().format('MM-DD-YYYY-h-mm-ss');
   const receiptPath = path.join(directory, `receipt-${timeStamp}.png`);
 
   return receiptPath;
